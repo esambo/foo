5fe65b893faae87cf92f209f2ca41c0f9b9d127a
E. Sambo <news@emmanuelsambo.com>
2017-01-30 21:27:19 -0600

    Change schema format from Ruby to SQL

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,10 @@ module Foo
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Use SQL instead of Active Record's schema dumper when creating the test database.
+    # This is necessary if your schema can't be completely dumped by the schema dumper,
+    # like if you have constraints or database-specific column types
+    config.active_record.schema_format = :sql
   end
 end
